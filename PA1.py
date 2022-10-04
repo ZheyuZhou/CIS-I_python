40c430f0fa90bc064bd6cfaa7f4a1af5c1fd5bc0
ZheyuZhou <70003965+ZheyuZhou@users.noreply.github.com>
2022-10-04 15:27:16 -0400

    Input data & defs
    Update input data and create definitions for PA1 Q1 to Q3

---
+++ PA1.py
@@ -0,0 +1,21 @@
+# Imports
+
+import numpy as np
+import pandas as pd
+
+
+# Functions
+
+# PA_1 Q1
+def Three_D_point():
+    return 0
+
+def Rotation():
+    return 0
+
+def Frame_Transform():
+    return 0
+
+# PA_1 Q3
+def Pivot_Calibration():
+    return 0
