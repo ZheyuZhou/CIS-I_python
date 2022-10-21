64b992e8be0426326af105495a9e491665ca98e5
ZheyuZhou <70003965+ZheyuZhou@users.noreply.github.com>
2022-10-21 16:49:56 -0400

    Update PA2.py

--- PA2.py
+++ PA2.py
@@ -33,3 +33,64 @@ import pandas as pd
 # W. McKinney, Pandas, Pandas. (2022). https://pandas.pydata.org/ (accessed October 12, 2022). 
 # from tqdm import tqdm_gui
 
+#######################################################################################
+#######################################################################################
+############### Data Import  ##########################################################
+#######################################################################################
+#######################################################################################
+# Name = np.array(['debug-a', 'debug-b', 'debug-c', 'debug-d', 'debug-e', 'debug-f', 'unknown-g', 'unknown-h' , 'unknown-i', 'unknown-j', 'unknown-k'])
+Name = np.array(['debug-a'])
+name = ''
+for nm in Name:
+
+    # calbody
+    # Read from TXT
+    df_pa2_calbody = pd.read_csv('pa2_student_data\pa2-'+nm+'-calbody.txt',
+    header=None, names=['N_d','N_a','N_c','Name_CALBODY'])
+    # W. McKinney, PANDAS.READ_CSV#, Pandas.read_csv - Pandas 1.5.0 Documentation. (2022). https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html (accessed October 12, 2022). 
+
+    calbody = df_pa2_calbody[['N_d','N_a','N_c']].to_numpy()    
+    # W. McKinney, Pandas.dataframe.to_numpy#, Pandas.DataFrame.to_numpy - Pandas 1.5.0 Documentation. (2022). https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_numpy.html (accessed October 12, 2022). 
+
+    # Get num
+    num_calbody = calbody[0]
+    num_calbody_d = int(num_calbody[0])
+    num_calbody_a = int(num_calbody[1])
+    num_calbody_c = int(num_calbody[2])
+
+    # Trim to get d, a, c
+    calbody_d = calbody[1:(1+num_calbody_d),:]
+    calbody_a = calbody[(1+num_calbody_d):(1+num_calbody_d+num_calbody_a),:]
+    calbody_c = calbody[(1+num_calbody_d+num_calbody_a):(1+num_calbody_d+num_calbody_a+num_calbody_c),:]
+
+
+    # calreadings
+    # Read from TXT
+    df_pa2_calreadings = pd.read_csv('pa2_student_data\pa2-'+nm+'-calreadings.txt', 
+    header=None, names=['N_D','N_A','N_C','N_Frame','Name_CALREADING'])
+
+    calreadings = df_pa2_calreadings[['N_D','N_A','N_C','N_Frame']].to_numpy()
+    
+    num_calreadings = calreadings[0]
+
+    # get num
+    num_calreadings_D = int(num_calreadings[0])
+    num_calreadings_A = int(num_calreadings[1])
+    num_calreadings_C = int(num_calreadings[2])
+    num_calreadings_Frame = int(num_calreadings[3])
+
+    num_calreadings_len = num_calreadings_D + num_calreadings_A + num_calreadings_C
+
+    # Trim to get D, A, C
+    calreadings_D = np.zeros((num_calreadings_Frame,num_calreadings_D,3))
+    calreadings_A = np.zeros((num_calreadings_Frame,num_calreadings_A,3))
+    calreadings_C = np.zeros((num_calreadings_Frame,num_calreadings_C,3))
+    # C. Harris, Numpy.zeros#, Numpy.zeros - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.zeros.html (accessed October 12, 2022). 
+
+    for i in range (num_calreadings_Frame):
+        I = i*num_calreadings_len
+        calreadings_D[i] = calreadings[(1) + I : (1+num_calreadings_D) + I , :-1]
+        calreadings_A[i] = calreadings[(1+num_calreadings_D) + I : (1+num_calreadings_D+num_calreadings_A) + I,:-1]
+        calreadings_C[i] = calreadings[(1+num_calreadings_D+num_calreadings_A) + I : (1+num_calreadings_D+num_calreadings_A+num_calreadings_C) + I,:-1]
+
+    
