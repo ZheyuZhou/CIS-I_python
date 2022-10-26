d7f670a6ef7d98ac485349d483029d562a8412d9
ZheyuZhou <70003965+ZheyuZhou@users.noreply.github.com>
2022-10-25 23:06:43 -0400

    Update PA2.py
    Debug C_expected

--- PA2.py
+++ PA2.py
@@ -34,6 +34,191 @@ import pandas as pd
 # from tqdm import tqdm_gui
 import math
 
+
+
+
+#######################################################################################
+#######################################################################################
+############### Functions  ############################################################
+#######################################################################################
+#######################################################################################
+def Cloudregistration(a,A):
+
+    # Calc average
+    a_bar = np.mean(a, axis=0)
+
+    A_bar = np.mean(A, axis=0)
+    # D. Gupta, Numpy.mean#, Numpy.mean - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.mean.html (accessed October 12, 2022). 
+    # Calc difference
+    a_tilde = []
+    for a_ in a:
+        a_tilde.append(a_bar - a_)
+    
+    A_tilde = []
+    for A_ in A:
+        A_tilde.append(A_bar - A_)
+
+    # Get H matrix
+    H = np.zeros((3,3))
+
+    for num_markers in range(len(A)): # len(A) = len(a) becaus the len is num of marker
+        d_tx_D_tx = a_tilde[num_markers][0]*A_tilde[num_markers][0]
+        d_tx_D_ty = a_tilde[num_markers][0]*A_tilde[num_markers][1]
+        d_tx_D_tz = a_tilde[num_markers][0]*A_tilde[num_markers][2]
+
+        d_ty_D_tx = a_tilde[num_markers][1]*A_tilde[num_markers][0]
+        d_ty_D_ty = a_tilde[num_markers][1]*A_tilde[num_markers][1]
+        d_ty_D_tz = a_tilde[num_markers][1]*A_tilde[num_markers][2]
+
+        d_tz_D_tx = a_tilde[num_markers][2]*A_tilde[num_markers][0]
+        d_tz_D_ty = a_tilde[num_markers][2]*A_tilde[num_markers][1]
+        d_tz_D_tz = a_tilde[num_markers][2]*A_tilde[num_markers][2]
+
+        H += np.array([
+            [d_tx_D_tx, d_tx_D_ty, d_tx_D_tz],
+            [d_ty_D_tx, d_ty_D_ty, d_ty_D_tz],
+            [d_tz_D_tx, d_tz_D_ty, d_tz_D_tz]
+        ])
+        # C. Harris, Numpy.array#, Numpy.array - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.array.html (accessed October 12, 2022). 
+
+    # Treat H and Get G
+    Tr_H = np.array([[np.trace(H)]])
+    
+    H23 = H[1][2]
+    H32 = H[2][1]
+    H31 = H[2][0]
+    H13 = H[0][2]
+    H12 = H[0][1]
+    H21 = H[1][0]
+
+    
+    Delta = np.array([
+        [H23-H32],
+        [H31-H13],
+        [H12-H21]
+    ])
+    # transpose the matrix
+    Delta_T = Delta.T
+    
+    H_HT_TrH_I = H + H.T - np.eye(3)*np.trace(H)
+    # C. Harris, Numpy.trace#, Numpy.trace - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.trace.html (accessed October 12, 2022). 
+    # Combine two matrix by horizonal
+    G1 = np.hstack((Tr_H, Delta_T))
+    G2 = np.hstack((Delta, H_HT_TrH_I))
+    # C. Harris, Numpy.hstack#, Numpy.hstack - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.hstack.html (accessed October 12, 2022). 
+    # Combine two matrix by vertical
+    G = np.vstack((G1,G2))
+    # M. Bussonnier, Numpy.vstack#, Numpy.vstack - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.vstack.html (accessed October 12, 2022). 
+
+    # Eig-val Decomp to get Eig-vec corresponds to largest(first) Eig-val
+    eig_val, eig_vec = np.linalg.eig(G)
+    # I. Polat, Numpy.linalg.eig#, Numpy.linalg.eig - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.linalg.eig.html (accessed October 12, 2022). 
+    # Choose the biggest eigenvalue and corresponding eigenvector
+    max_eig_val_index = np.argmax(eig_val)
+    # D. Gupta, Numpy.argmax#, Numpy.argmax - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.argmax.html (accessed October 13, 2022). 
+    Qk = eig_vec[:,max_eig_val_index]
+    # print(eig_vec, 'eig_vec')
+    # print(Qk, 'Qk')
+
+    q0 = Qk[0]
+    q1 = Qk[1]
+    q2 = Qk[2]
+    q3 = Qk[3]
+
+
+
+    # Plug in Unit Quaternion(Eig-vec above) to get Rotation Matrix
+    R = np.array([
+        [q0**2 + q1**2 - q2**2 - q3**2, 2*(q1*q2 - q0*q3), 2*(q1*q3 + q0*q2)],
+        [2*(q1*q2 + q0*q3), q0**2 - q1**2 + q2**2 - q3**2, 2*(q2*q3 - q0*q1)],
+        [2*(q1*q3 - q0*q2), 2*(q2*q3 + q0*q1), q0**2 - q1**2 - q2**2 + q3**2]
+    ])
+
+    # Calc translation
+    p = A_bar - R@a_bar
+    
+    p = np.reshape(p, (3,1))
+    # D. Gupta, Numpy.reshape#, Numpy.reshape - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.reshape.html (accessed October 13, 2022). 
+
+    # Organize to get Transformation
+    F1 = np.hstack((R,p))
+    F2 = np.array([0,0,0,1])
+    F = np.vstack((F1,F2))
+
+    return F 
+
+
+def ScaleToBox(q):
+    qT = q.T
+    qx = qT[0]
+    qy = qT[1]
+    qz = qT[2]
+
+    qx_min = np.min(qx)
+    qy_min = np.min(qy)
+    qz_min = np.min(qz)
+    
+    qx_max = np.max(qx)
+    qy_max = np.max(qy)
+    qz_max = np.max(qz)
+
+    ux = (qx-qx_min) / (qx_max-qx_min)
+    uy = (qy-qy_min) / (qy_max-qy_min)
+    uz = (qz-qz_min) / (qz_max-qz_min)
+
+    if ux >= 0 and ux <= 1 and uy >= 0 and uy <= 1 and uz >= 0 and uz <= 1:
+        u = np.array([ux, uy, uz])
+        return u
+    else:
+        print('u not in well functioning range!!!!')
+        u = np.array([ux, uy, uz])
+        return u
+
+
+def B_5_Poly(q, k):
+    u = ScaleToBox(q)
+    v = 1 - u
+    N = 5
+    bionomial_coef = math.comb(N,k)
+    B_N_k = bionomial_coef * u**(N-k) * v**(k)
+    return B_N_k
+
+def B_5_x_Poly(q,k):
+    B_N_k_x = B_5_Poly(q, k)[0]
+    return B_N_k_x
+
+def B_5_y_Poly(q,k):
+    B_N_k_y = B_5_Poly(q, k)[1]
+    return B_N_k_y
+
+def B_5_z_Poly(q,k):
+    B_N_k_z = B_5_Poly(q, k)[2]
+    return B_N_k_z
+
+def Tensor_Form(q):
+    F = np.zeros(6,6,6)
+    for i in range(6):
+        for j in range(6):
+            for k in range(6):
+                B_N_i_x = B_5_x_Poly(q,i)
+                # B_N_i_y = B_5_y_Poly(q,i)
+                # B_N_i_z = B_5_z_Poly(q,i)
+
+                # B_N_j_x = B_5_x_Poly(q,j)
+                B_N_j_y = B_5_y_Poly(q,j)
+                # B_N_j_z = B_5_z_Poly(q,j)
+
+                # B_N_j_x = B_5_x_Poly(q,j)
+                # B_N_j_y = B_5_y_Poly(q,j)
+                B_N_j_z = B_5_z_Poly(q,j)
+
+                F[i][j][k] = B_N_i_x * B_N_j_y * B_N_j_z
+    F = np.ndarray.flatten(F)
+
+    return F
+
+
+
 #######################################################################################
 #######################################################################################
 ############### Data Import  ##########################################################
@@ -202,121 +387,6 @@ for nm in Name:
         K = k*num_em_nav_len
         em_nav_G[k] = em_nav[(1) + K : (1+num_em_nav_G) + K , : ]
 
-
-    
-#######################################################################################
-#######################################################################################
-############### Functions  ############################################################
-#######################################################################################
-#######################################################################################
-def Cloudregistration(a,A):
-
-    # Calc average
-    a_bar = np.mean(a, axis=0)
-
-    A_bar = np.mean(A, axis=0)
-    # D. Gupta, Numpy.mean#, Numpy.mean - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.mean.html (accessed October 12, 2022). 
-    # Calc difference
-    a_tilde = []
-    for a_ in a:
-        a_tilde.append(a_bar - a_)
-    
-    A_tilde = []
-    for A_ in A:
-        A_tilde.append(A_bar - A_)
-
-    # Get H matrix
-    H = np.zeros((3,3))
-
-    for num_markers in range(len(A)): # len(A) = len(a) becaus the len is num of marker
-        d_tx_D_tx = a_tilde[num_markers][0]*A_tilde[num_markers][0]
-        d_tx_D_ty = a_tilde[num_markers][0]*A_tilde[num_markers][1]
-        d_tx_D_tz = a_tilde[num_markers][0]*A_tilde[num_markers][2]
-
-        d_ty_D_tx = a_tilde[num_markers][1]*A_tilde[num_markers][0]
-        d_ty_D_ty = a_tilde[num_markers][1]*A_tilde[num_markers][1]
-        d_ty_D_tz = a_tilde[num_markers][1]*A_tilde[num_markers][2]
-
-        d_tz_D_tx = a_tilde[num_markers][2]*A_tilde[num_markers][0]
-        d_tz_D_ty = a_tilde[num_markers][2]*A_tilde[num_markers][1]
-        d_tz_D_tz = a_tilde[num_markers][2]*A_tilde[num_markers][2]
-
-        H += np.array([
-            [d_tx_D_tx, d_tx_D_ty, d_tx_D_tz],
-            [d_ty_D_tx, d_ty_D_ty, d_ty_D_tz],
-            [d_tz_D_tx, d_tz_D_ty, d_tz_D_tz]
-        ])
-        # C. Harris, Numpy.array#, Numpy.array - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.array.html (accessed October 12, 2022). 
-
-    # Treat H and Get G
-    Tr_H = np.array([[np.trace(H)]])
-    
-    H23 = H[1][2]
-    H32 = H[2][1]
-    H31 = H[2][0]
-    H13 = H[0][2]
-    H12 = H[0][1]
-    H21 = H[1][0]
-
-    
-    Delta = np.array([
-        [H23-H32],
-        [H31-H13],
-        [H12-H21]
-    ])
-    # transpose the matrix
-    Delta_T = Delta.T
-    
-    H_HT_TrH_I = H + H.T - np.eye(3)*np.trace(H)
-    # C. Harris, Numpy.trace#, Numpy.trace - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.trace.html (accessed October 12, 2022). 
-    # Combine two matrix by horizonal
-    G1 = np.hstack((Tr_H, Delta_T))
-    G2 = np.hstack((Delta, H_HT_TrH_I))
-    # C. Harris, Numpy.hstack#, Numpy.hstack - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.hstack.html (accessed October 12, 2022). 
-    # Combine two matrix by vertical
-    G = np.vstack((G1,G2))
-    # M. Bussonnier, Numpy.vstack#, Numpy.vstack - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.vstack.html (accessed October 12, 2022). 
-
-    # Eig-val Decomp to get Eig-vec corresponds to largest(first) Eig-val
-    eig_val, eig_vec = np.linalg.eig(G)
-    # I. Polat, Numpy.linalg.eig#, Numpy.linalg.eig - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.linalg.eig.html (accessed October 12, 2022). 
-    # Choose the biggest eigenvalue and corresponding eigenvector
-    max_eig_val_index = np.argmax(eig_val)
-    # D. Gupta, Numpy.argmax#, Numpy.argmax - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.argmax.html (accessed October 13, 2022). 
-    Qk = eig_vec[:,max_eig_val_index]
-    # print(eig_vec, 'eig_vec')
-    # print(Qk, 'Qk')
-
-    q0 = Qk[0]
-    q1 = Qk[1]
-    q2 = Qk[2]
-    q3 = Qk[3]
-
-
-
-    # Plug in Unit Quaternion(Eig-vec above) to get Rotation Matrix
-    R = np.array([
-        [q0**2 + q1**2 - q2**2 - q3**2, 2*(q1*q2 - q0*q3), 2*(q1*q3 + q0*q2)],
-        [2*(q1*q2 + q0*q3), q0**2 - q1**2 + q2**2 - q3**2, 2*(q2*q3 - q0*q1)],
-        [2*(q1*q3 - q0*q2), 2*(q2*q3 + q0*q1), q0**2 - q1**2 - q2**2 + q3**2]
-    ])
-
-    # Calc translation
-    p = A_bar - R@a_bar
-    
-    p = np.reshape(p, (3,1))
-    # D. Gupta, Numpy.reshape#, Numpy.reshape - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.reshape.html (accessed October 13, 2022). 
-
-    # Organize to get Transformation
-    F1 = np.hstack((R,p))
-    F2 = np.array([0,0,0,1])
-    F = np.vstack((F1,F2))
-
-    return F 
-
-
-
-
 #######################################################################################
 #######################################################################################
 ############### Calculation  ##########################################################
@@ -325,28 +395,28 @@ def Cloudregistration(a,A):
 
 # F_D Transformation between optical tracker and EM tracker coordinates through all data frames
 F_D = []
-for i in range (len(calbody_d)):
+for i in range (len(calreadings_D)):
     F_D.append(Cloudregistration(calbody_d,calreadings_D[i]))
     # print('F_D')
-
+# print(len(calreadings_D), len(calbody_d))
 F_D = np.array([F_D])[0]
 # print(F_D)
-# print(np.shape(F_D),' shape F_D')
+print(np.shape(F_D),' shape F_D')
 
 # F_A transformation between calibration object and optical trackercoordinates through all data frames
 F_A = []
-for j in range (len(calbody_a)):
+for j in range (len(calreadings_A)):
     F_A.append(Cloudregistration(calbody_a,calreadings_A[j]))
     # print('F_A')
 F_A = np.array([F_A])[0]
 # print(F_A)
-# print(np.shape(F_A))
+print(np.shape(F_A),' shape F_A')
 
 # Calc C_vector expected for each data frame
 C_vec_expected = []
 C_vec_expected_d = []
-# print(h_calbody_c)
-# print(np.shape(h_calbody_c))
+# print(calbody_c)
+# print(np.shape(calbody_c))
 
 # Make c into 4x1 matrix
 c_I = np.ones((len(calbody_c), 1))
@@ -368,80 +438,5 @@ for d in range(len(F_D)):
         C = C[0:3]
         C_vec_expected.append(C)
 # print(C_vec_expected, 'C_expected')
-# print(np.shape(C_vec_expected), 'C_expected shape')
+print(np.shape(C_vec_expected), 'C_expected shape')
 
-def ScaleToBox(q):
-    qT = q.T
-    qx = qT[0]
-    qy = qT[1]
-    qz = qT[2]
-
-    qx_min = np.min(qx)
-    qy_min = np.min(qy)
-    qz_min = np.min(qz)
-    
-    qx_max = np.max(qx)
-    qy_max = np.max(qy)
-    qz_max = np.max(qz)
-
-    ux = (qx-qx_min) / (qx_max-qx_min)
-    uy = (qy-qy_min) / (qy_max-qy_min)
-    uz = (qz-qz_min) / (qz_max-qz_min)
-
-    if ux >= 0 and ux <= 1 and uy >= 0 and uy <= 1 and uz >= 0 and uz <= 1:
-        u = np.array([ux, uy, uz])
-        return u
-    else:
-        print('u not in well functioning range!!!!')
-        u = np.array([ux, uy, uz])
-        return u
-
-def B_5_Poly(q, k):
-    u = ScaleToBox(q)
-    v = 1 - u
-    N = 5
-    bionomial_coef = math.comb(N,k)
-    B_N_k = bionomial_coef * u**(N-k) * v**(k)
-    return B_N_k
-
-def B_5_x_Poly(q,k):
-    B_N_k_x = B_5_Poly(q, k)[0]
-    return B_N_k_x
-
-def B_5_y_Poly(q,k):
-    B_N_k_y = B_5_Poly(q, k)[1]
-    return B_N_k_y
-
-def B_5_z_Poly(q,k):
-    B_N_k_z = B_5_Poly(q, k)[2]
-    return B_N_k_z
-
-def Tensor_Form(q):
-    F = np.zeros(6,6,6)
-    for i in range(6):
-        for j in range(6):
-            for k in range(6):
-                B_N_i_x = B_5_x_Poly(q,i)
-                # B_N_i_y = B_5_y_Poly(q,i)
-                # B_N_i_z = B_5_z_Poly(q,i)
-
-                # B_N_j_x = B_5_x_Poly(q,j)
-                B_N_j_y = B_5_y_Poly(q,j)
-                # B_N_j_z = B_5_z_Poly(q,j)
-
-                # B_N_j_x = B_5_x_Poly(q,j)
-                # B_N_j_y = B_5_y_Poly(q,j)
-                B_N_j_z = B_5_z_Poly(q,j)
-
-                F[i][j][k] = B_N_i_x * B_N_j_y * B_N_j_z
-    F = np.ndarray.flatten(F)
-
-    return F
-
-
-
-    
-
-
-# def distortion_correction():
-    
