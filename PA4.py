91b9a84a6f7f3d27ee0cef0fb8002f862fd7785e
ZheyuZhou <zheyuzhou99@gmail.com>
2022-12-08 01:06:59 -0500

    Update PA4.py

--- PA4.py
+++ PA4.py
@@ -24,122 +24,8 @@ import pa345DataImport as dataimport
 import findtipdk as findtipdk
 import BruteSearch as BruteSearch
 import BoundingSphereSearch as BoundingSphereSearch
-
-# # find the tip related to B
-# def findTip(F_Ak, F_Bk, tipA):
-#     tipA4 = np.append(tipA, np.array([1]))
-#     # D. Gupta, Numpy.append#, Numpy.append - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.mean.html (accessed November 12, 2022). 
-#     tipA4 = np.reshape(tipA4, (4,1))
-#     # D. Gupta, Numpy.reshape#, Numpy.reshape - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.reshape.html (accessed October 13, 2022). 
-#     d_k = []
-#     for i in range(len(F_Ak)):
-#         F_aki = F_Ak[i]
-#         F_bki = F_Bk[i]
-#         d_k.append(np.linalg.inv(F_bki)@F_aki@tipA4)
-#         # I. Polat, Numpy.linalg.inv#, Numpy.linalg.inv - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/gene
-#     d_k = np.array(d_k)
-#     return d_k
-
-# # Find closet triangle point
-# def Find_closet_triangle_point(a,p,q,r):
-#     k = np.array([[q[0]-p[0],r[0]-p[0]],[q[1]-p[1],r[1]-p[1]],[q[2]-p[2],r[2]-p[2]]])
-#     a = np.reshape(a[0:3], (1,3))[0]
-#     # D. Gupta, Numpy.reshape#, Numpy.reshape - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.reshape.html (accessed October 13, 2022). 
-#     ld = np.linalg.lstsq(k,a - p, rcond=None)[0][0]
-#     # I. Polat, Numpy.linalg.lstsq#, Numpy.linalg.lstsq - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html (accessed October 13, 2022). 
-#     u = np.linalg.lstsq(k,a - p, rcond=None)[0][1]
-#     c = p + ld * (q-p) + u * (r-p)
-#     # Discussion of the point out of the triangle
-#     if ld>=0 and u>=0 and ld+u<=1:
-#         cnew = c
-#     elif ld<0:
-#         cnew = ProjectOnSegment(c,r,p)
-#     elif u<0:
-#         cnew = ProjectOnSegment(c,p,q)
-#     elif ld + u >1:
-#         cnew = ProjectOnSegment(c,q,r)
-#     return cnew
-
-# # Find the projection of the vector to the plane
-# def ProjectOnSegment(c,p,q):
-#     ld = (c-p).dot(q-p)/(q-p).dot(q-p)
-#     lds = max(0,min(ld,1))
-#     cs = p+lds*(q-p)
-#     return cs
-
-# # linear search through all the triangles (brute)
-# def Find_closet_points(Meshpoints,Triangle_record,s_kn):
-#     # Meshpoints is N vertices,Triangle_record is Vertex indices
-#     c_total = []
-#     d_total = []
-#     # Doing the loop for the all points of triangle 
-#     for i in range(len(Triangle_record)):
-#         n1 = int(Triangle_record[i][0])
-#         n2 = int(Triangle_record[i][1])
-#         n3 = int(Triangle_record[i][2])
-#         p = Meshpoints[n1]
-#         q = Meshpoints[n2]
-#         r = Meshpoints[n3]
-#         # Find the closet point of the triangle 
-#         c_k = Find_closet_triangle_point(s_kn,p,q,r)
-#         c_total.append(c_k)
-#         s_kn = np.reshape(s_kn[0:3], (1,3))[0]
-#         dis = np.linalg.norm(c_k - s_kn)
-#         # D. Gupta, Numpy.linalg.norm - NumPy v1.23 Manual. (2022).https://numpy.org/doc/stable/reference/generated/numpy.linalg.norm.html
-#         d_total.append(dis)
-#         # storage of the data
-#     c_total = np.array(c_total)
-#     d_total = np.array(d_total)
-#     return c_total,d_total
-
-# # Select the real cloest points from all the points we . 
-# def Find_closet_mesh_point(c_total,s_kn):
-#     dist = []
-#     s_kn = np.reshape(s_kn[0:3], (1,3))[0]
-#     for i in range(len(c_total)):
-#         dist.append(np.linalg.norm(c_total[i] - s_kn))
-#     dist = np.array(dist)
-#     closet_dist_index = np.argmin(dist)
-#      # D. Gupta, Numpy.argmax and argmin #, Numpy.argmin - NumPy v1.23 Manual. (2022). https://numpy.org/doc/stable/reference/generated/numpy.arg
-#     return c_total[closet_dist_index]
-
-# # Bounding sphere of three points triangle
-# def Finding_centralpoint_radius(a,b,c):
-#     f = (a+b)/2
-#     u = a - f
-#     v = c - f
-#     d = np.cross(np.cross(u,v),u)
-#     ld = max(0, (v.dot(v) - u.dot(u))/(2*d.dot(v-u)))
-#     q = f + ld*d
-#     # q is the central point of sphere
-#     p = np.linalg.norm(a-q)
-#     # p is the radius
-#     return q,p
-
-
-# # Find the c_k with the bounding sphere judgement
-# def Closet_bounding_points(Meshpoints,Triangle_record,s_kn):
-#     s_kn3 = np.reshape(s_kn[0:3], (1,3))[0]
-#     bound = np.linalg.norm(s_kn3)
-#     for i in range(len(Triangle_record)):
-#         n1 = int(Triangle_record[i][0])
-#         n2 = int(Triangle_record[i][1])
-#         n3 = int(Triangle_record[i][2])
-#         p = Meshpoints[n1]
-#         q = Meshpoints[n2]
-#         r = Meshpoints[n3]
-#         ph = Finding_centralpoint_radius(p,q,r)[1]
-#         ct = Finding_centralpoint_radius(p,q,r)[0]
-#         # calculate the radius of the sphere 
-#         d1 = np.linalg.norm(s_kn3 - ct) - ph
-#         if d1 < bound:
-#             c_k1 = Find_closet_triangle_point(s_kn,p,q,r)
-#             d2 = np.linalg.norm(s_kn3 - c_k1)
-#             if d2 < bound:
-#                 c_k = c_k1
-#                 bound = d2
-        
-#     return c_k
+import FindCloestPoint as FCP
+import IterativeClosestPoint as ICP
 
 
 
@@ -239,37 +125,14 @@ if __name__ == '__main__':
         s_k_frame.append(s_k)
     
 
-    # Brute force
-    # c_closest_frame = []
-    # for s_k in s_k_frame:
-    #     c_closest_sample = []
-    #     for s in s_k:
-    #         c_total,_ = Find_closet_points(pa3_vertices,pa3_triangles,s)
-    #         c_closest = Find_closet_mesh_point(c_total,s)
-    #         c_closest_sample.append(c_closest)
-    #     c_closest_sample = np.array(c_closest_sample)
-    #     c_closest_frame.append(c_closest_sample)
-    # c_closest_frame = np.array(c_closest_frame)
-
     # c_closest_frame = BruteSearch.BruteSearch(s_k_frame, pa3_vertices,pa3_triangles)
-    
-    
-
-    # # Bounding
-    # c_closest_frame = []
-    # for s_k in s_k_frame:
-    #     c_closest_sample = []
-    #     for s in s_k:
-    #         c_closest = Closet_bounding_points(pa3_vertices,pa3_triangles,s)
-    #         c_closest_sample.append(c_closest)
-    #     c_closest_sample = np.array(c_closest_sample)
-    #     c_closest_frame.append(c_closest_sample)
-    # c_closest_frame = np.array(c_closest_frame)
 
     c_closest_frame = BoundingSphereSearch.BoundingSphereSearch(s_k_frame, pa3_vertices,pa3_triangles)
 
-    # print(c_closest_frame)
+    print(c_closest_frame)
     # print(d_k3_frame)
+
+
 
     #######################################################################################
     #######################################################################################
